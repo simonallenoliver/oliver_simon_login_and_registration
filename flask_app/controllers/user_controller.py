5f8c6b44551a5742cad32d0b3b3cc33b5a530e1c
SimonAllenOliver <simonallenoliver@gmail.com>
2024-01-24 12:49:51 -0600

    finish

--- flask_app/controllers/user_controller.py
+++ flask_app/controllers/user_controller.py
@@ -1,20 +1,71 @@
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, session
 from flask_app import app
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app.models.user_model import User
+from flask import flash
+
+from flask_bcrypt import Bcrypt        
+bcrypt = Bcrypt(app)
 
 
+# home page with a register form and a login form 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
+
+# here we have the registration post method route. the if statement uses a User class method to check 
+# the form to see if the user inputs are acceptable. If they are acceptable the pw_hash encrypts the password,
+# and the following dictionary requests the data from the form to then send to the database with the 
+# User class method called save. On success, it redirects to the new user's page (this last part needs implemented still)  
 @app.route('/register_new_user', methods=['POST'])
 def register_new_user():
-    # if there are errors:
-    # We call the staticmethod on User model to validate
-    if not User.validate_user(request.form):
-        # redirect to the route where the user form is rendered.
+    if User.is_valid(request.form):
+        pw_hash = bcrypt.generate_password_hash(request.form['password'])
+        user_data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "password" : pw_hash,
+        "confirm_password" : request.form['confirm_password']
+    }
+        User.save(user_data)
+        session['user_id'] = user_data.id
+        return redirect('/user_page')
+    return redirect('/')
+
+
+#individual user pages (need to add ID)
+@app.route('/user_page')
+def user_page():
+    if 'user_id' in session:
+        print("this is line 43", session['user_id'])
+        # user_id = session['user_id']
+        data = {'user_id': session['user_id']}
+        print("this is line 46", data["user_id"])
+        current_user = User.get_user_by_id(data)
+        return render_template("user_page.html", current_user = current_user)
+    else: return redirect('/')
+
+
+@app.route('/login',methods=['POST'])
+def login():
+    data = {'email' : request.form['email']}
+    user_in_db = User.check_database(data)
+    if not user_in_db:
+        flash("invalid email", "login")
         return redirect('/')
-    # else no errors:
-    User.save(request.form)
-    return redirect("/")
+    if not bcrypt.check_password_hash(user_in_db.password, request.form['password']):
+        flash("invalid password", "login")
+        return redirect('/')
+    session['user_id'] = user_in_db.id
+    return redirect('/user_page')
+
+
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
--- flask_app/models/user_model.py
+++ flask_app/models/user_model.py
@@ -1,6 +1,10 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash 
+import re
 
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+# the User class holds all the data stored in our users table in the db
 class User:
     def __init__(self, data):
             self.id = data["id"]
@@ -13,19 +17,79 @@ class User:
             
 
     @classmethod
+    def check_database(cls, data):
+        query = "SELECT * FROM users WHERE email = %(email)s"
+        
+        result = connectToMySQL("login_and_registration_db").query_db(query, data)
+        
+        if len(result) < 1:
+            return False
+        return cls(result[0])
+
+    @classmethod
+    def get_user_by_id(cls, data):
+        query = "SELECT * FROM users WHERE id = %(user_id)s;"
+        result = connectToMySQL("login_and_registration_db").query_db(query,data)
+        return cls(result[0])
+
+
+#the save_user class method 
+    @classmethod
     def save(cls,data):
-        query = "INSERT into surveys (name,location,language,comments) VALUES (%(name)s,%(location)s,%(language)s,%(comments)s);"
+        query = "INSERT into users (first_name,last_name,email,password) VALUES (%(first_name)s,%(last_name)s,%(email)s,%(password)s);"
         return connectToMySQL('login_and_registration_db').query_db(query,data)
          
+    @classmethod
+    def check_password(cls, data):
+        query = "SELECT password FROM users WHERE email = %(email)s"
+        
+        results = connectToMySQL("login_and_registration_db").query_db(query, data)
+        
+        return results
+    
 
 
     @staticmethod
-    def validate_user(user):
+    def is_valid(user):
         is_valid = True # we assume this is true
         if len(user['first_name']) < 2:
-            flash("First name must be at least 2 characters.")
+            flash("First name must be at least 2 characters.", "registration")
             is_valid = False
         if len(user['last_name']) < 2:
-            flash("Last name must be at least 2 characters.")
+            flash("Last name must be at least 2 characters.", "registration")
             is_valid = False
+        if len(user['email']) < 5:
+            flash("email must be at least 5 characters.", "registration")
+            is_valid = False
+
+#password validations
+        if len(user['password']) < 5:
+            flash("Password must be at least 5 characters.", "registration")
+            is_valid = False
+        if(re.search('[0-9]', user['password']) == None ):
+            flash("Password must include a number", "registration")
+            is_valid = False
+         
+        if(re.search('[A-Z]', user['password']) == None ):
+            flash("Password must include an upper case letter", "registration")
+            is_valid = False
+                      
+        if (user['password'] != user['confirm_password']):
+            flash("Passwords do not match!", "registration")
+            is_valid = False
+        #after checking all the above ifs return is_valid
+    
+
+        if not EMAIL_REGEX.match(user['email']): 
+            flash("Please enter a valid email address.", "registration")
+            is_valid = False
+
+        if (re.fullmatch(EMAIL_REGEX, user['email'])):
+            this_user = {'email': user['email']}
+            results = User.check_database(this_user)
+            if len(results) != 0:
+                flash('There is an existing account associated with this email address; please login.', "registration")
+                is_valid = False
+
+
         return is_valid
